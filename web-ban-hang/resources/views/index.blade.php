becbd8df78a30431924d4b529c720da3c5db2cdd
nguyenhuutuyen920 <tuyennguyen27082002@gmail.com>
2024-05-21 11:12:29 +0700

    he

---
+++ web-ban-hang/resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    ...
+    {{-- <link href="/css/app.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+      <a class="navbar-brand" href="">tindog</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <!--<a class="navbar-brand" href="">tindog</a>-->
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                  <!--Contact-->
+                  <a class="nav-link" href>Contact</a>
+              </li>
+              <li class="nav-item">
+                  <!--Contact-->
+                  <a class="nav-link" href>Pricing</a>
+              </li>
+              <li class="nav-item">
+                  <!--Contact-->
+                  <a class="nav-link" href>Download</a>
+              </li>
+          </ul>
+      </div>
+    </nav>
+    <script src="js/app.js" charset="utf-8"></script>
+  </body>
+</html>
