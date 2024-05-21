1253a836ae27269f757e59546d492689be37cba7
nguyenhuutuyen920 <129480086+nguyenhuutuyen920@users.noreply.github.com>
2024-05-21 09:41:43 +0700

    fhf

---
+++ introduce-company/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/bootstrap-4.6.2-dist/css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="d-flex justify-content-center">aaaaaa</div>
+
+    <script src="/bootstrap-4.6.2-dist/js/bootstrap.js"></script>
+</body>
+</html>
