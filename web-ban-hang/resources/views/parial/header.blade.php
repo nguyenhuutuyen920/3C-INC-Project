7f46ccf588aae3f1c01964a253d1c4ee68c35dc3
nguyenhuutuyen920 <tuyennguyen27082002@gmail.com>
2024-05-22 22:31:59 +0700

    new

--- web-ban-hang/resources/views/parial/header.blade.php
+++ web-ban-hang/resources/views/parial/header.blade.php
@@ -1,51 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('fonts/fonts/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/css/owl.carousel.min.css') }}">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand mr-2" href="{{ route('home')}}"><img src="img/logo.png" style="width:40px;height:40px" alt=""></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse ml-3" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item ml-2">
-                <a class="nav-link text-dark h5 m-0" href="{{ route('home')}}">Trang Chủ</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle text-dark h5 m-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sản Phẩm
-              </a>
-              <div class="dropdown-menu mt-auto" style="margin-left: -155px;background-color:aliceblue; width: 300px; height:150px" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Thiết Bị Tự Động Hóa</a>
-                <a class="dropdown-item" href="#">Thiết Bị Công Nghệ Thông Tin</a>
-                <a class="dropdown-item" href="#">Thiết Bị Viễn THông Xây Lắp</a>
-                <a class="dropdown-item" href="#">Thiết Bị Thí Nghiệm Đo Lường</a>
-              </div>
-            </li>
-            <li class="nav-item ml-2">
-                <a class="nav-link text-dark h5 m-0" href="#">Giải Pháp</a>
-            </li>
-            <li class="nav-item ml-2">
-                <a class="nav-link text-dark h5 m-0" href="#">Dịch Vụ</a>
-            </li>
-            <li class="nav-item ml-2">
-                <a class="nav-link text-dark h5 m-0" href="#">Dự Án</a>
-            </li>
-            <li class="nav-item ml-2">
-                <a class="nav-link text-dark h5 m-0" href="{{ route('contact')}}">Liên Hệ</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0 mr-5">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/css/bootstrap.min.css') }}">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('css/css/style.css') }}">
+
+    <title>Website Menu #4</title>
+  </head>
+  <body>
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
         </div>
-      </nav>
+        <div class="site-mobile-menu-body"></div>
+      </div>
 
-    <script src="js/js/jquery.min.js"></script>
-    <script src="js/js/popper.min.js"></script>
-    <script src="js/js/bootstrap.min.js"></script>
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <a href="#" class=""><span class="mr-2  icon-envelope-open-o"></span> <span class="d-none d-md-inline-block">info@yourdomain.com</span></a>
+              <span class="mx-md-2 d-inline-block"></span>
+              <a href="#" class=""><span class="mr-2  icon-phone"></span> <span class="d-none d-md-inline-block">1+ (234) 5678 9101</span></a>
 
-    <!-- jQuery, Popper.js, and Bootstrap JS -->
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
+              <div class="float-right">
+                <a href="#" class=""><span class="mr-2  icon-twitter"></span> <span class="d-none d-md-inline-block">Twitter</span></a>
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="#" class=""><span class="mr-2  icon-facebook"></span> <span class="d-none d-md-inline-block">Facebook</span></a>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+        <div class="container">
+          <div class="row align-items-center position-relative">
+            <div class="site-logo">
+              <a href="{{ route('home') }}" class="text-black"><span class="text-primary">Brand</span></a>
+            </div>
+
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="#home-section" class="nav-link">Home</a></li>
+                  <li><a href="#services-section" class="nav-link">Services</a></li>
+                  <li class="has-children">
+                    <a href="#about-section" class="nav-link">About Us</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#team-section" class="nav-link">Team</a></li>
+                      <li><a href="#pricing-section" class="nav-link">Pricing</a></li>
+                      <li><a href="#faq-section" class="nav-link">FAQ</a></li>
+                      <li class="has-children">
+                        <a href="#">More Links</a>
+                        <ul class="dropdown">
+                          <li><a href="#">Menu One</a></li>
+                          <li><a href="#">Menu Two</a></li>
+                          <li><a href="#">Menu Three</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="#why-us-section" class="nav-link">Why Us</a></li>
+                  <li><a href="#testimonials-section" class="nav-link">Testimonials</a></li>
+                  <li><a href="#blog-section" class="nav-link">Blog</a></li>
+                  <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+        </div>
+      </header>
+
+    <script src="{{ asset('js/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/js/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('js/js/main.js') }}"></script>
+  </body>
+</html>
